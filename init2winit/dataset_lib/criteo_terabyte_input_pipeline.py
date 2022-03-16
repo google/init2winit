507abcb6073bbfaf2c8c2d84811943e614324bce
Rohan Anil <rohananil@google.com>
2022-03-15 23:42:15 -0700

    Cleanup Criteo input pipeline.
    PiperOrigin-RevId: 434964170

--- init2winit/dataset_lib/criteo_terabyte_input_pipeline.py
+++ init2winit/dataset_lib/criteo_terabyte_input_pipeline.py
@@ -19,8 +19,8 @@ import functools
 import itertools
 import os
 
-from absl import logging
 from init2winit.dataset_lib.data_utils import Dataset
+import jax
 from ml_collections.config_dict import config_dict
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -31,16 +31,11 @@ CRITEO1TB_DEFAULT_HPARAMS = config_dict.ConfigDict(dict(
     input_shape=(13 + 26,),
     train_file_path=os.path.join(CRITEO1TB_FILE_PATH, 'train/train*'),
     eval_file_path=os.path.join(CRITEO1TB_FILE_PATH, 'eval/eval*'),
-    num_train_datasets=8192,
     # TODO(eamid): find the exact train_size
     train_size=4e9,
-    num_eval_datasets=1072,
-    parallelism=16,
-    dataset_index=0,
-    dataset_num_shards=4
 ))
 CRITEO1TB_METADATA = {
-    'apply_one_hot_in_loss': False,
+    'apply_one_hot_in_loss': True,
 }
 
 
@@ -59,27 +54,14 @@ class CriteoTsvReader(object):
                num_dense_features=None,
                vocab_sizes=None,
                batch_size=None,
-               dataset_index=0,
-               dataset_num_shards=None,
                is_training=True,
-               parallelism=1,
-               num_datasets=None):
+               parallelism=16):
     self._file_path = file_path
     self._num_dense_features = num_dense_features
     self._vocab_sizes = vocab_sizes
     self._batch_size = batch_size
-    self._dataset_index = dataset_index
-    self._dataset_num_shards = dataset_num_shards
     self._is_training = is_training
     self._parallelism = parallelism
-
-    num_files = len(tf.io.gfile.glob(file_path))
-    if num_datasets > num_files:
-      raise ValueError('There must be at least one file per dataset.')
-    if num_datasets * parallelism > num_files:
-      logging.warning(
-          'Reducing dataset parallelism due to insufficient number of files.')
-      self._parallelism = num_files // num_datasets
 
   def __call__(self):
     batch_size = self._batch_size
@@ -97,7 +79,7 @@ class CriteoTsvReader(object):
       num_labels = 1
       num_dense = len(int_defaults)
       features = {}
-      features['targets'] = tf.reshape(fields[0], [batch_size, 1])
+      features['targets'] = tf.reshape(fields[0], [batch_size])
 
       int_features = []
       for idx in range(num_dense):
@@ -116,29 +98,20 @@ class CriteoTsvReader(object):
       return features
 
     filenames = tf.data.Dataset.list_files(self._file_path, shuffle=False)
-    filenames = filenames.shard(self._dataset_num_shards, self._dataset_index)
+    index = jax.process_index()
+    num_hosts = jax.process_count()
+    ds = filenames.shard(num_hosts, index)
 
-    def make_dataset(ds_index, repeat=True):
-      ds = filenames.shard(self._parallelism, ds_index)
-      if repeat:
-        ds = ds.repeat()
-      ds = ds.interleave(
-          tf.data.TextLineDataset,
-          cycle_length=16,
-          block_length=batch_size // 8,
-          num_parallel_calls=8,
-          deterministic=False)
-      ds = ds.batch(batch_size, drop_remainder=True)
-      ds = ds.map(_parse_example_fn, num_parallel_calls=16)
-      return ds
-
-    ds_indices = tf.data.Dataset.range(self._parallelism)
-    ds = ds_indices.interleave(
-        functools.partial(make_dataset, repeat=self._is_training),
-        cycle_length=self._parallelism,
-        block_length=1,
-        num_parallel_calls=self._parallelism,
+    if self._is_training:
+      ds = ds.repeat()
+    ds = ds.interleave(
+        tf.data.TextLineDataset,
+        cycle_length=16,
+        block_length=batch_size // 8,
+        num_parallel_calls=8,
         deterministic=False)
+    ds = ds.batch(batch_size, drop_remainder=True)
+    ds = ds.map(_parse_example_fn, num_parallel_calls=16)
     ds = ds.prefetch(tf.data.AUTOTUNE)
     return ds
 
@@ -157,22 +130,14 @@ def get_criteo1tb(unused_shuffle_rng,
       num_dense_features=hps.num_dense_features,
       vocab_sizes=hps.vocab_sizes,
       batch_size=batch_size,
-      dataset_index=hps.dataset_index,
-      dataset_num_shards=hps.dataset_num_shards,
-      is_training=True,
-      parallelism=hps.parallelism,
-      num_datasets=hps.num_train_datasets)
+      is_training=True)
   train_iterator_fn = lambda: tfds.as_numpy(train_dataset())
   eval_train_dataset = CriteoTsvReader(
       file_path=hps.train_file_path,
       num_dense_features=hps.num_dense_features,
       vocab_sizes=hps.vocab_sizes,
       batch_size=eval_batch_size,
-      dataset_index=hps.dataset_index,
-      dataset_num_shards=hps.dataset_num_shards,
-      is_training=False,
-      parallelism=hps.parallelism,
-      num_datasets=hps.num_train_datasets)
+      is_training=False)
   eval_train_epoch = functools.partial(
       convert_to_numpy_iterator_fn, tf_dataset_fn=eval_train_dataset)
   eval_dataset = CriteoTsvReader(
@@ -180,11 +145,7 @@ def get_criteo1tb(unused_shuffle_rng,
       num_dense_features=hps.num_dense_features,
       vocab_sizes=hps.vocab_sizes,
       batch_size=eval_batch_size,
-      dataset_index=hps.dataset_index,
-      dataset_num_shards=hps.dataset_num_shards,
-      is_training=False,
-      parallelism=hps.parallelism,
-      num_datasets=hps.num_eval_datasets)
+      is_training=False)
   eval_iterator_fn = functools.partial(
       convert_to_numpy_iterator_fn, tf_dataset_fn=eval_dataset)
   # pylint: disable=unreachable
