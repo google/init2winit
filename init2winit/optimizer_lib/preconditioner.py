7eb0a3f8d3551ec0295007998213bff9661339a6
init2winit Team <no-reply@google.com>
2022-05-04 05:51:15 -0700

    Add API for deconstructing preconditioners in Kitchen Sink.
    PiperOrigin-RevId: 446433017

---
+++ init2winit/optimizer_lib/preconditioner.py
@@ -0,0 +1,191 @@
+# coding=utf-8
+# Copyright 2022 The init2winit Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Modularizing optimizer preconditioners.
+
+TL;DR: `optax.GradientTransformation`s, but initializing a bit differently and
+abusing the `updates` that get passed from one transformation to the next.
+
+We split up preconditioners into three "stages":
+1. Produce the variables to be accumulated.
+2. Accumulate the variables.
+3. Use `updates` and outputs from above two stages to compute final update.
+
+Each stage is an `optax.GradientTransformation`, but must follow certain rules:
+
+1. `updates` passed into `tx.update` must be of the form
+
+    ```
+    {
+      'updates': updates,  # The original `updates` passed from training loop.
+      'variables': {},     # A dictionary of variables to accumulate.
+      'moments': {},       # Moments computed from variables.
+      'output': None,      # Final list of outputs.
+    }
+    ```
+
+2. After stage 1, `variables` is populated, after stage 2, `moments` is
+   populated, after stage 3, `output` is populated.
+
+NOTE: `preconditioner` is a wrapper `optax.GradientTransformation` that takes
+three `optax.GradientTransformation`s adhering to above rules and correctly
+initializes each and forms the `updates` dictionary.
+"""
+
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+import jax
+import jax.numpy as jnp
+import optax
+
+
+def preconditioner(
+    grad_transformer,
+    accumulator,
+    updater,
+    moment_creator_args,
+    accumulator_args,
+    updater_args,
+) -> optax.GradientTransformation:
+  """Generic precondition update function."""
+
+  grad_transformer = grad_transformer(**moment_creator_args)
+  accumulator = accumulator(**accumulator_args)
+  updater = updater(**updater_args)
+
+  def init(params: optax.Params) -> optax.OptState:
+    """`init` function."""
+    grads_state = grad_transformer.init(params)
+
+    # NOTE(dsuo): assumes params and updates have the same shape.
+    updates = {'updates': params, 'variables': {}}
+    grads, _ = grad_transformer.update(updates, grads_state, params)
+
+    # NOTE(dsuo): assume accumulator only needs `gradients`.
+    accumulator_state = accumulator.init(grads['variables'])
+
+    updater_state = updater.init(params)
+    return (grads_state, accumulator_state, updater_state)
+
+  def update(updates, state, params=None):
+    """`update` function."""
+    updates = {
+        'updates': updates,
+        'variables': {},
+        'moments': {},
+        'output': None
+    }
+    new_state = []
+    for s, transform in zip(state, [grad_transformer, accumulator, updater]):
+      updates, new_s = transform.update(updates, s, params)
+      new_state.append(new_s)
+
+    return updates['output'], tuple(new_state)
+
+  return optax.GradientTransformation(init, update)
+
+
+def nth_power(
+    power: Union[int, Tuple[int]] = 2) -> optax.GradientTransformation:
+  """Create nth power(s) from gradients."""
+
+  if not hasattr(power, '__iter__'):
+    power = [power]
+
+  for p in power:
+    if p != int(p):
+      raise ValueError(f'Currently we only support integer orders; got {p}.')
+
+  def init(params: optax.Params) -> optax.OptState:
+    del params
+    return None
+
+  def update(
+      updates: optax.Updates,
+      state: optax.OptState,
+      params: Optional[optax.Params] = None
+  ) -> Tuple[optax.Updates, optax.OptState]:
+    del params
+
+    for p in power:
+      if p == 1:
+        updates['variables'][str(int(p))] = updates['updates']
+      else:
+        gradients = jax.tree_map(lambda x: x**p, updates['updates'])  # pylint: disable=cell-var-from-loop
+        updates['variables'][str(int(p))] = gradients
+
+    return updates, state
+
+  return optax.GradientTransformation(init, update)
+
+
+def ema_accumulator(decay: float = 0.999,
+                    debias: bool = False) -> optax.GradientTransformation:
+  """Create accumulator that computes EMA on all updates."""
+
+  def init(params: optax.Params) -> optax.OptState:
+    return (jax.tree_map(jnp.zeros_like, params), jnp.array(0, dtype=jnp.int32))
+
+  def update(
+      updates: optax.Updates,
+      state: optax.OptState,
+      params: Optional[optax.Params] = None
+  ) -> Tuple[optax.Updates, optax.OptState]:
+    del params
+
+    moments, count = state
+    update_fn = lambda g, t: (1 - decay) * g + decay * t
+    moments = jax.tree_map(update_fn, updates['variables'], moments)
+
+    count = count + jnp.array(1, dtype=jnp.int32)
+    beta = jnp.array(1, dtype=jnp.int32) - decay**count
+    updates['moments'] = moments if not debias else jax.tree_map(
+        lambda t: t / beta.astype(t.dtype), moments)
+
+    return updates, (moments, count)
+
+  return optax.GradientTransformation(init, update)
+
+
+def rexp_updater(
+    exponent: float = 0.5,
+    eps: float = 1e-8,
+    eps_root: float = 0.0,
+    moment: int = 2,
+    use_accumulated_gradient: bool = False,
+) -> optax.GradientTransformation:
+  """Apply an update function."""
+  def init(params: optax.Params) -> optax.OptState:
+    del params
+    return None
+
+  def update(
+      updates: optax.Updates,
+      state: optax.OptState,
+      params: Optional[optax.Params] = None
+  ) -> Tuple[optax.Updates, optax.OptState]:
+    del params
+    grads = updates['updates'] if not use_accumulated_gradient else updates[
+        'moments']['1']
+
+    updates['output'] = jax.tree_multimap(
+        lambda u, v: u / (jnp.power(v + eps_root, exponent) + eps), grads,
+        updates['moments'][str(moment)])
+
+    return updates, state
+
+  return optax.GradientTransformation(init, update)
---
+++ init2winit/optimizer_lib/test_preconditioner.py
@@ -0,0 +1,136 @@
+# coding=utf-8
+# Copyright 2022 The init2winit Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for search_subspace."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import chex
+from init2winit.optimizer_lib import preconditioner
+from init2winit.optimizer_lib import transform
+import jax
+import jax.numpy as jnp
+import optax
+
+
+class GradTransformTest(parameterized.TestCase):
+  """Test grad transforms."""
+
+  @parameterized.product(
+      power=[1, 2, 4, [1, 2], [0, 3]],
+      seed=[0, 1],
+  )
+  def test_nth_power(self, power, seed):
+    """Test nth_power."""
+    grads = jax.random.uniform(jax.random.PRNGKey(seed), (10, 10))
+    grads = {'updates': grads, 'variables': {}, 'moments': {}, 'output': None}
+
+    actual_power = power
+    if not isinstance(power, list):
+      actual_power = [actual_power]
+
+    actual_grads = {
+        str(int(o)): jax.tree_map(lambda x: x**o, grads['updates'])  # pylint: disable=cell-var-from-loop
+        for o in actual_power
+    }
+    nth_grads, _ = preconditioner.nth_power(power).update(grads, None)
+
+    chex.assert_trees_all_close(actual_grads, nth_grads['variables'])
+
+
+class AccumulatorTest(parameterized.TestCase):
+  """Test accumulators."""
+
+  @parameterized.product(
+      decay=[0.1, 0.5, 0.9, 0.999],
+      debias=[True, False],
+  )
+  def test_ema_accumulator(self, decay, debias):
+    """Test ema_accumulator."""
+    grads = jax.random.uniform(jax.random.PRNGKey(0), (10, 10))
+    grads = {'updates': grads, 'variables': {}, 'moments': {}, 'output': None}
+
+    nth_grads = preconditioner.nth_power(power=[1, 2])
+    accumulator = preconditioner.ema_accumulator(decay, debias)
+
+    grads, _ = nth_grads.update(grads, None)
+    state = accumulator.init(grads['variables'])
+
+    for i in range(5):
+      moments, count = state
+      grads = jax.random.uniform(jax.random.PRNGKey(i + 1), (10, 10))
+      grads = {'updates': grads, 'variables': {}, 'moments': {}, 'output': None}
+      grads, _ = nth_grads.update(grads, None)
+      updates, state = accumulator.update(grads, state)
+
+      actual = jax.tree_map(lambda g, t: (1 - decay) * g + decay * t,
+                            grads['variables'], moments)
+      if debias:
+        count += jnp.array(1, dtype=jnp.int32)
+        beta = jnp.array(1, dtype=jnp.int32) - decay ** count
+        actual = jax.tree_map(lambda t: t / beta.astype(t.dtype), actual)  # pylint: disable=cell-var-from-loop
+
+      chex.assert_trees_all_close(updates['moments'], actual)
+
+
+class PreconditionerTest(parameterized.TestCase):
+  """Test preconditioner decompositions."""
+
+  @chex.variants(with_jit=True, with_pmap=True, without_jit=True)
+  @parameterized.product(
+      decay=[0.999, 0.9, 0.1],
+      eps=[1e-8, 1e-4, 1e-1],
+      eps_root=[1e-8, 1e-4, 0.0],
+      debias=[True, False],
+  )
+  def test_precondition_by_rms(self, decay, eps, eps_root, debias):
+    """Test precondition_by_rms."""
+
+    actual_rms = transform.precondition_by_rms(
+        decay=decay, eps=eps, eps_root=eps_root, debias=debias)
+
+    decon_rms = preconditioner.preconditioner(preconditioner.nth_power,
+                                              preconditioner.ema_accumulator,
+                                              preconditioner.rexp_updater,
+                                              {'power': 2}, {
+                                                  'decay': decay,
+                                                  'debias': debias
+                                              }, {
+                                                  'eps': eps,
+                                                  'eps_root': eps_root,
+                                              })
+
+    params = jax.random.uniform(jax.random.PRNGKey(0), (10, 10))
+
+    init = self.variant(decon_rms.init)
+    update = self.variant(decon_rms.update)
+
+    actual_state = actual_rms.init(params)
+    decon_state = init(params)
+
+    for i in range(5):
+      grads = jax.random.uniform(jax.random.PRNGKey(i + 1), (10, 10))
+
+      actual_updates, actual_state = actual_rms.update(grads, actual_state)
+      decon_updates, decon_state = update(grads, decon_state)
+
+      actual_params = optax.apply_updates(params, actual_updates)
+      decon_params = optax.apply_updates(params, decon_updates)
+
+      chex.assert_trees_all_close(actual_params, decon_params, atol=1e-4)
+
+
+if __name__ == '__main__':
+  absltest.main()
