Fix fastmri SSIM calculation. Image/target are normalized for training/inference, but must be unnormalized prior to computing SSIM.
PiperOrigin-RevId: 448226121

init2winit/dataset_lib/fastmri_dataset.py
@@ -134,7 +134,13 @@ def _process_example(kspace, kspace_shape, target, target_shape, volume_max,
   norm_target = (target - mean) / std
   target = tf.clip_by_value(norm_target, -6, 6)
 
-  return {'inputs': image, 'targets': target, 'volume_max': volume_max}
+  return {
+      'inputs': image,
+      'targets': target,
+      'mean': mean,
+      'std': std,
+      'volume_max': volume_max
+  }
 
 
 def _h5_to_examples(path):

init2winit/model_lib/losses.py
@@ -186,6 +186,8 @@ def ctc_loss(logits, logit_paddings, labels, label_paddings, blank_id=0):
 def weighted_unnormalized_mean_absolute_error(logits,
                                               targets,
                                               weights=None,
+                                              mean=None,
+                                              std=None,
                                               **kwargs):
   """Computes weighted mean absolute error (L1) loss.
 
@@ -194,13 +196,25 @@ def weighted_unnormalized_mean_absolute_error(logits,
       keeping argument naming consistent.
     targets: (batch,) + input.shape float array.
     weights: None or array of shape (batch,)
-    **kwargs: Extra arguments that are ignored.
+    mean: (batch,) mean of original images.
+    std: (batch,) std of original images.
+    **kwargs: extra keyword args that are ignored.
 
   Returns:
     L1 loss computed per example with shape (batch,)
   """
   del kwargs
 
+  if mean is None:
+    mean = jnp.zeros(logits.shape[0])
+
+  if std is None:
+    std = jnp.ones(logits.shape[0])
+
+  mean = mean.reshape((-1,) + (1,) * (len(logits.shape) - 1))
+  std = std.reshape((-1,) + (1,) * (len(logits.shape) - 1))
+  logits = logits * std - mean
+  targets = targets * std - mean
   loss = jnp.abs(logits - targets).mean(axis=tuple(range(1, logits.ndim)))
 
   if weights is not None:

init2winit/model_lib/metrics.py
@@ -374,7 +374,7 @@ def structural_similarity(im1,
   return jnp.mean(s.at[pad:-pad, pad:-pad].get())
 
 
-def ssim(logits, targets, weights=None, volume_max=None):
+def ssim(logits, targets, weights=None, mean=None, std=None, volume_max=None):
   """Computes example-wise structural similarity for a batch.
 
   NOTE(dsuo): we use the same (default) arguments to `structural_similarity`
@@ -383,7 +383,9 @@ def ssim(logits, targets, weights=None, volume_max=None):
   Args:
    logits: (batch,) + input.shape float array.
    targets: (batch,) + input.shape float array.
-   weights: None or array of shape (batch,)
+   weights: None or array of shape (batch,).
+   mean: (batch,) mean of original images.
+   std: (batch,) std of original images.
    volume_max: (batch,) of the volume max for the volumes each example came
     from.
 
@@ -391,8 +393,18 @@ def ssim(logits, targets, weights=None, volume_max=None):
     Structural similarity computed per example, shape [batch, ...].
   """
   if volume_max is None:
-    volume_max = np.zeros(logits.shape[0])
+    volume_max = jnp.zeros(logits.shape[0])
 
+  if mean is None:
+    mean = jnp.zeros(logits.shape[0])
+
+  if std is None:
+    std = jnp.ones(logits.shape[0])
+
+  mean = mean.reshape((-1,) + (1,) * (len(logits.shape) - 1))
+  std = std.reshape((-1,) + (1,) * (len(logits.shape) - 1))
+  logits = logits * std - mean
+  targets = targets * std - mean
   ssims = jax.vmap(structural_similarity)(logits, targets, volume_max)
 
   if weights is not None: