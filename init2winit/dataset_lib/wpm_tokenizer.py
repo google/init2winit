bcc77868422e5e9382e458be9ace11941679b580
Sourabh Medapati <smedapati@google.com>
2022-05-18 14:16:35 -0700

    Internal
    PiperOrigin-RevId: 449574919

---
+++ init2winit/dataset_lib/wpm_tokenizer.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+# Copyright 2022 The init2winit Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Word piece model tokenizer for tokenizing text datasets."""
+from typing import Optional
+
+from absl import logging
+import numpy as np
+import tensorflow as tf
+
+try:
+  from lingvo.core import wpm_encoder  # pylint: disable=g-import-not-at-top
+except ModuleNotFoundError:
+  logging.exception('\n\nUnable to import lingvo WPM Encoder.\n\n')
+
+
+class WpmTokenizer():
