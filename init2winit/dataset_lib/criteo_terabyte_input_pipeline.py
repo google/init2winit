e1d090b0fcf594b0e3e7bc32e47999e5be57028a
Zachary Nado <znado@google.com>
2022-08-07 20:12:30 -0700

    Fixing padding for criteo multi-process evals.
    PiperOrigin-RevId: 465942638

--- init2winit/dataset_lib/criteo_terabyte_input_pipeline.py
+++ init2winit/dataset_lib/criteo_terabyte_input_pipeline.py
@@ -14,14 +14,15 @@
 # limitations under the License.
 
 """Data loader for pre-processed Criteo data."""
-
 import functools
-import itertools
+import math
 import os
 
-from init2winit.dataset_lib.data_utils import Dataset
+from absl import logging
+from init2winit.dataset_lib import data_utils
 import jax
 from ml_collections.config_dict import config_dict
+import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
@@ -37,6 +38,35 @@ CRITEO1TB_DEFAULT_HPARAMS = config_dict.ConfigDict(dict(
 CRITEO1TB_METADATA = {
     'apply_one_hot_in_loss': True,
 }
+
+
+@tf.function
+def _parse_example_fn(num_dense_features, vocab_sizes, example):
+  """Parser function for pre-processed Criteo TSV records."""
+  label_defaults = [[0.0]]
+  int_defaults = [[0.0] for _ in range(num_dense_features)]
+  categorical_defaults = [[0] for _ in range(len(vocab_sizes))]
+  record_defaults = label_defaults + int_defaults + categorical_defaults
+  fields = tf.io.decode_csv(
+      example, record_defaults, field_delim='\t', na_value='-1')
+
+  num_labels = 1
+  features = {}
+  features['targets'] = tf.reshape(fields[0], (-1,))
+
+  int_features = []
+  for idx in range(num_dense_features):
+    int_features.append(fields[idx + num_labels])
+  int_features = tf.stack(int_features, axis=1)
+
+  cat_features = []
+  for idx in range(len(vocab_sizes)):
+    cat_features.append(
+        tf.cast(fields[idx + num_dense_features + num_labels], dtype=tf.int32))
+  cat_features = tf.cast(
+      tf.stack(cat_features, axis=1), dtype=int_features.dtype)
+  features['inputs'] = tf.concat([int_features, cat_features], axis=1)
+  return features
 
 
 def _criteo_tsv_reader(
@@ -62,43 +92,11 @@ def _criteo_tsv_reader(
   Returns:
     A tf.data.Dataset object.
   """
-
-  @tf.function
-  def _parse_example_fn(example):
-    """Parser function for pre-processed Criteo TSV records."""
-    label_defaults = [[0.0]]
-    int_defaults = [[0.0] for _ in range(num_dense_features)]
-    categorical_defaults = [[0] for _ in range(len(vocab_sizes))]
-    record_defaults = label_defaults + int_defaults + categorical_defaults
-    fields = tf.io.decode_csv(
-        example, record_defaults, field_delim='\t', na_value='-1')
-
-    num_labels = 1
-    num_dense = len(int_defaults)
-    features = {}
-    features['targets'] = tf.reshape(fields[0], [batch_size])
-
-    int_features = []
-    for idx in range(num_dense):
-      int_features.append(fields[idx + num_labels])
-    int_features = tf.stack(int_features, axis=1)
-
-    cat_features = []
-    for idx in range(len(vocab_sizes)):
-      cat_features.append(
-          tf.cast(fields[idx + num_dense + num_labels], dtype=tf.int32))
-    cat_features = tf.cast(
-        tf.stack(cat_features, axis=1), dtype=int_features.dtype)
-    features['inputs'] = tf.concat([int_features, cat_features], axis=1)
-    features['weights'] = tf.ones(
-        shape=(features['inputs'].shape[0],), dtype=features['inputs'].dtype)
-    return features
-
-  filenames = tf.data.Dataset.list_files(file_path, shuffle=False)
+  ds = tf.data.Dataset.list_files(file_path, shuffle=False)
   index = jax.process_index()
   num_hosts = jax.process_count()
-  ds = filenames.shard(num_hosts, index)
 
+  ds = ds.shard(num_hosts, index)
   if is_training:
     ds = ds.repeat()
   ds = ds.interleave(
@@ -107,14 +105,51 @@ def _criteo_tsv_reader(
       block_length=batch_size // 8,
       num_parallel_calls=128,
       deterministic=False)
-  ds = ds.batch(batch_size, drop_remainder=True)
-  ds = ds.map(_parse_example_fn, num_parallel_calls=16)
+  ds = ds.batch(batch_size, drop_remainder=is_training)
+  parse_fn = functools.partial(
+      _parse_example_fn, num_dense_features, vocab_sizes)
+  ds = ds.map(parse_fn, num_parallel_calls=16)
   ds = ds.prefetch(tf.data.AUTOTUNE)
   return ds
 
 
-def convert_to_numpy_iterator_fn(num_batches, tf_dataset):
-  return itertools.islice(tfds.as_numpy(tf_dataset), num_batches)
+def _convert_to_numpy_iterator_fn(
+    num_batches, per_host_eval_batch_size, tf_dataset, split_size):
+  """Make eval iterator. This function is called at the start of each eval."""
+  # Some hosts could see different numbers of examples, which in some cases
+  # could lead to some hosts not having enough examples to make the same number
+  # of batches. This makes pmap hang because it is waiting for a batch that will
+  # never come from the host with less data.
+  #
+  # We assume that all files have the same number of examples in them, and while
+  # this may not always be true, it dramatically simplifies/speeds up the logic
+  # because the alternative is to run the same iterator (without data file
+  # sharding) on each host, skipping (num_hosts - 1) / num_hosts batches.
+  #
+  # Any final partial batches are padded to be the full batch size, so we can
+  # treat them all as being the same batch size.
+  num_hosts = jax.process_count()
+  num_batches_in_split = math.ceil(
+      split_size / (per_host_eval_batch_size * num_hosts))
+  if (num_batches is None or num_batches < 0 or
+      num_batches > num_batches_in_split):
+    logging.info('Setting num_batches to %d.', num_batches_in_split)
+    num_batches = num_batches_in_split
+
+  iterator = iter(tfds.as_numpy(tf_dataset))
+  zeros_batch = None
+  for _ in range(num_batches):
+    try:
+      batch = next(iterator)
+    except StopIteration:
+      if zeros_batch is None:
+        zeros_batch = jax.tree_map(
+            lambda x: np.zeros_like(x, dtype=x.dtype), batch)
+      yield zeros_batch
+      continue
+    batch = data_utils.maybe_pad_batch(
+        batch, desired_batch_size=per_host_eval_batch_size)
+    yield batch
 
 
 def get_criteo1tb(unused_shuffle_rng,
@@ -147,7 +182,10 @@ def get_criteo1tb(unused_shuffle_rng,
       batch_size=per_host_eval_batch_size,
       is_training=False)
   eval_train_epoch = functools.partial(
-      convert_to_numpy_iterator_fn, tf_dataset=eval_train_dataset)
+      _convert_to_numpy_iterator_fn,
+      per_host_eval_batch_size=per_host_eval_batch_size,
+      tf_dataset=eval_train_dataset,
+      split_size=hps.train_size)
   eval_dataset = _criteo_tsv_reader(
       file_path=hps.eval_file_path,
       num_dense_features=hps.num_dense_features,
@@ -155,7 +193,10 @@ def get_criteo1tb(unused_shuffle_rng,
       batch_size=per_host_eval_batch_size,
       is_training=False)
   eval_iterator_fn = functools.partial(
-      convert_to_numpy_iterator_fn, tf_dataset=eval_dataset)
+      _convert_to_numpy_iterator_fn,
+      per_host_eval_batch_size=per_host_eval_batch_size,
+      tf_dataset=eval_dataset,
+      split_size=hps.valid_size)
   # pylint: disable=unreachable
   def test_epoch(*args, **kwargs):
     del args
@@ -163,5 +204,5 @@ def get_criteo1tb(unused_shuffle_rng,
     return
     yield  # This yield is needed to make this a valid (null) iterator.
   # pylint: enable=unreachable
-  return Dataset(train_iterator_fn, eval_train_epoch, eval_iterator_fn,
-                 test_epoch)
+  return data_utils.Dataset(
+      train_iterator_fn, eval_train_epoch, eval_iterator_fn, test_epoch)
