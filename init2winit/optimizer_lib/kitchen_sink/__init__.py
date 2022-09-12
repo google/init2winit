a18d2bd18564dab5eec6800c7496bea075c7b3b8
init2winit Team <no-reply@google.com>
2022-09-12 08:39:49 -0700

    Expose all public functions and classes from `kitchen_sink`'s `transform.py`.
    PiperOrigin-RevId: 473756899

--- init2winit/optimizer_lib/kitchen_sink/__init__.py
+++ init2winit/optimizer_lib/kitchen_sink/__init__.py
@@ -17,7 +17,56 @@
 
 from init2winit.optimizer_lib.kitchen_sink._src.alias import nadamw
 from init2winit.optimizer_lib.kitchen_sink._src.core import kitchen_sink
+from init2winit.optimizer_lib.kitchen_sink._src.transform import bias_correction
+from init2winit.optimizer_lib.kitchen_sink._src.transform import BiasCorrectionState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import clip_updates
+from init2winit.optimizer_lib.kitchen_sink._src.transform import first_moment_ema
+from init2winit.optimizer_lib.kitchen_sink._src.transform import nesterov
+from init2winit.optimizer_lib.kitchen_sink._src.transform import polyak_averaging
+from init2winit.optimizer_lib.kitchen_sink._src.transform import Polyak_AveragingState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import polyak_hb
+from init2winit.optimizer_lib.kitchen_sink._src.transform import precondition_by_amsgrad
+from init2winit.optimizer_lib.kitchen_sink._src.transform import precondition_by_layered_adaptive_rms
+from init2winit.optimizer_lib.kitchen_sink._src.transform import precondition_by_rms
+from init2winit.optimizer_lib.kitchen_sink._src.transform import precondition_by_rss
+from init2winit.optimizer_lib.kitchen_sink._src.transform import precondition_by_yogi
+from init2winit.optimizer_lib.kitchen_sink._src.transform import PreconditionByLayeredAdaptiveRMSState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import PreconditionByRssState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import PreconditionBySecondMomentCoordinateWiseState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import sanitize_values
+from init2winit.optimizer_lib.kitchen_sink._src.transform import scale_by_adam
+from init2winit.optimizer_lib.kitchen_sink._src.transform import scale_by_amsgrad
+from init2winit.optimizer_lib.kitchen_sink._src.transform import scale_by_learning_rate
+from init2winit.optimizer_lib.kitchen_sink._src.transform import scale_by_nadam
+from init2winit.optimizer_lib.kitchen_sink._src.transform import ScaleByAdamState
+from init2winit.optimizer_lib.kitchen_sink._src.transform import ScaleByAMSGradState
 
 __version__ = '0.0.1'
 
-__all__ = ('nadamw', 'kitchen_sink')
+__all__ = (
+    'nadamw',
+    'kitchen_sink',
+    'bias_correction',
+    'BiasCorrectionState',
+    'clip_updates',
+    'first_moment_ema',
+    'nesterov',
+    'polyak_averaging',
+    'Polyak_AveragingState',
+    'polyak_hb',
+    'precondition_by_amsgrad',
+    'precondition_by_layered_adaptive_rms',
+    'precondition_by_rms',
+    'precondition_by_rss',
+    'precondition_by_yogi',
+    'PreconditionByLayeredAdaptiveRMSState',
+    'PreconditionByRssState',
+    'PreconditionBySecondMomentCoordinateWiseState',
+    'sanitize_values',
+    'scale_by_adam',
+    'scale_by_amsgrad',
+    'scale_by_learning_rate',
+    'scale_by_nadam',
+    'ScaleByAdamState',
+    'ScaleByAMSGradState',
+)
