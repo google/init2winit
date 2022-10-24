d203ee81717992acccdec7a54481dd5dd1ecec58
init2winit Team <no-reply@google.com>
2022-10-24 06:12:38 -0700

    Add get_fake_batch to UNetModel.
    PiperOrigin-RevId: 483359615

--- init2winit/model_lib/unet.py
+++ init2winit/model_lib/unet.py
@@ -304,3 +304,9 @@ class UNetModel(base_model.BaseModel):
         chans=self.hps.chans,
         num_pool_layers=self.hps.num_pool_layers,
         drop_prob=self.hps.drop_prob)
+
+  def get_fake_batch(self, hps):
+    fake_batch = [
+        jnp.zeros((hps.batch_size, *hps.input_shape), dtype=hps.model_dtype)
+    ]
+    return fake_batch
