49bfc6c444f5e059f3e4433a2eb1ba85f36f2845
init2winit Team <no-reply@google.com>
2022-10-25 08:37:06 -0700

    Add dataset API for getting a fake batch from the dataset.
    There are instances where we need to know the (typical) shape of a batch output from the dataset, including inputs, targets, and any metadata, but we do not want to full instantiate the pipeline and grab data from disk/network. The most immediate example is in testing.
    
    NOTES
    - This first CL simply declares the API and sets `fake_batch_getter` to None for all datasets since there are no dependencies yet.
    - As expected, tests pass.
    - We intend to implement this function for all datasets (and any future datasets), but will prioritize those datasets for the upcoming MLCommons competition.
    
    PiperOrigin-RevId: 483683289

--- init2winit/dataset_lib/datasets.py
+++ init2winit/dataset_lib/datasets.py
@@ -31,70 +31,73 @@ from init2winit.dataset_lib import small_image_datasets
 from init2winit.dataset_lib import translate_wmt
 from init2winit.dataset_lib import wikitext2
 
-_Dataset = collections.namedtuple('Dataset', ('getter', 'hparams', 'meta_data'))
+_Dataset = collections.namedtuple(
+    'Dataset', ('getter', 'hparams', 'meta_data', 'fake_batch_getter'))
 
 _ALL_DATASETS = {
     'mnist':
         _Dataset(small_image_datasets.get_mnist,
                  small_image_datasets.MNIST_HPARAMS,
-                 small_image_datasets.MNIST_METADATA),
+                 small_image_datasets.MNIST_METADATA, None),
     'mnist_autoencoder':
         _Dataset(small_image_datasets.get_mnist_autoencoder,
                  small_image_datasets.MNIST_AUTOENCODER_HPARAMS,
-                 small_image_datasets.MNIST_AUTOENCODER_METADATA),
+                 small_image_datasets.MNIST_AUTOENCODER_METADATA, None),
     'fashion_mnist':
         _Dataset(small_image_datasets.get_fashion_mnist,
                  small_image_datasets.FASHION_MNIST_HPARAMS,
-                 small_image_datasets.FASHION_MNIST_METADATA),
+                 small_image_datasets.FASHION_MNIST_METADATA, None),
     'cifar10':
         _Dataset(small_image_datasets.get_cifar10,
                  small_image_datasets.CIFAR10_DEFAULT_HPARAMS,
-                 small_image_datasets.CIFAR10_METADATA),
+                 small_image_datasets.CIFAR10_METADATA, None),
     'cifar100':
         _Dataset(small_image_datasets.get_cifar100,
                  small_image_datasets.CIFAR100_DEFAULT_HPARAMS,
-                 small_image_datasets.CIFAR100_METADATA),
+                 small_image_datasets.CIFAR100_METADATA, None),
     'criteo1tb':
         _Dataset(criteo_terabyte_input_pipeline.get_criteo1tb,
                  criteo_terabyte_input_pipeline.CRITEO1TB_DEFAULT_HPARAMS,
-                 criteo_terabyte_input_pipeline.CRITEO1TB_METADATA),
+                 criteo_terabyte_input_pipeline.CRITEO1TB_METADATA, None),
     'fake':
         _Dataset(fake_dataset.get_fake, fake_dataset.DEFAULT_HPARAMS,
-                 fake_dataset.METADATA),
+                 fake_dataset.METADATA, None),
     'fastmri':
         _Dataset(fastmri_dataset.get_fastmri, fastmri_dataset.DEFAULT_HPARAMS,
-                 fastmri_dataset.METADATA),
+                 fastmri_dataset.METADATA, None),
     'imagenet':
         _Dataset(imagenet_dataset.get_imagenet,
-                 imagenet_dataset.DEFAULT_HPARAMS, imagenet_dataset.METADATA),
+                 imagenet_dataset.DEFAULT_HPARAMS, imagenet_dataset.METADATA,
+                 None),
     'translate_wmt':
         _Dataset(translate_wmt.get_translate_wmt, translate_wmt.DEFAULT_HPARAMS,
-                 translate_wmt.METADATA),
+                 translate_wmt.METADATA, None),
     'librispeech':
         _Dataset(librispeech.get_librispeech, librispeech.DEFAULT_HPARAMS,
-                 librispeech.METADATA),
+                 librispeech.METADATA, None),
     'lm1b_v2':
-        _Dataset(lm1b_v2.get_lm1b, lm1b_v2.DEFAULT_HPARAMS, lm1b_v2.METADATA),
+        _Dataset(lm1b_v2.get_lm1b, lm1b_v2.DEFAULT_HPARAMS, lm1b_v2.METADATA,
+                 None),
     'mlperf_imagenet':
         _Dataset(mlperf_imagenet_dataset.get_mlperf_imagenet,
                  mlperf_imagenet_dataset.DEFAULT_HPARAMS,
-                 mlperf_imagenet_dataset.METADATA),
+                 mlperf_imagenet_dataset.METADATA, None),
     'svhn_no_extra':
         _Dataset(small_image_datasets.get_svhn_no_extra,
                  small_image_datasets.SVHN_NO_EXTRA_DEFAULT_HPARAMS,
-                 small_image_datasets.SVHN_NO_EXTRA_METADATA),
+                 small_image_datasets.SVHN_NO_EXTRA_METADATA, None),
     'nqm_noise':
         _Dataset(nqm_noise.get_nqm_noise, nqm_noise.NQM_HPARAMS,
-                 nqm_noise.NQM_METADATA),
+                 nqm_noise.NQM_METADATA, None),
     'ogbg_molpcba':
         _Dataset(ogbg_molpcba.get_ogbg_molpcba, ogbg_molpcba.DEFAULT_HPARAMS,
-                 ogbg_molpcba.METADATA),
+                 ogbg_molpcba.METADATA, None),
     'uniref50':
         _Dataset(proteins.get_uniref, proteins.DEFAULT_HPARAMS,
-                 proteins.METADATA),
+                 proteins.METADATA, None),
     'wikitext2':
         _Dataset(wikitext2.get_wikitext2, wikitext2.DEFAULT_HPARAMS,
-                 wikitext2.METADATA),
+                 wikitext2.METADATA, None),
 }
 
 
@@ -149,5 +152,30 @@ def get_dataset_meta_data(dataset_name):
   """
   try:
     return _ALL_DATASETS[dataset_name].meta_data
+  except KeyError:
+    raise ValueError('Unrecognized dataset: {}'.format(dataset_name)) from None
+
+
+def get_fake_batch(dataset_name):
+  """Maps dataset name to fake_batch_getter.
+
+  fake_batch_getter is a function that takes a `ConfigDict` of fully formed hps
+  (e.g., the output from `hyperparameters.build_hparams`)
+
+  Args:
+    dataset_name: (str) the name of the dataset.
+
+  Returns:
+    A function that takes a `ConfigDict` and returns a fake batch of the same
+    shape as a real batch from the dataset.
+  Raises
+    ValueError, if `dataset_name` is not in `_ALL_DATASETS` or the
+    `fake_batch_getter` is `None`.
+  """
+  try:
+    getter = _ALL_DATASETS[dataset_name].fake_batch_getter
+    if getter is None:
+      raise ValueError(
+          f'Fake batch getter not defined for dataset {dataset_name}') from None
   except KeyError:
     raise ValueError('Unrecognized dataset: {}'.format(dataset_name)) from None
