FastMRI and SSIM fixes. - Fix SSIM calculation by weighting per example SSIMs after handling `NaNs`. In the current state where this order is reversed the NaNs are weighted by `0` (still `NaN`) and then handled by being set to `-1`. The result of the bug is that all padding examples which have SSIM value `NaN` will contribute `-1` to the total SSIM, so the computed SSIM will appear lower. - Fix indexing bug in FastMRI dataset, the `end` index used for slicing should be set to the total number of files and not to `-1`. Slicing to `-1` will exclude the last file. Also correct the number of test examples, which was incorrectly changed previously to reflect the measured number of test examples. That measured number was incorrect due to the indexing bug.
PiperOrigin-RevId: 587742932

init2winit/model_lib/metrics.py
@@ -436,9 +436,6 @@ def ssim(logits, targets, weights=None, mean=None, std=None, volume_max=None):
   targets = targets * std + mean
   ssims = jax.vmap(structural_similarity)(logits, targets, volume_max)
 
-  if weights is not None:
-    ssims = ssims * weights
-
   # NOTE(dsuo): map NaN ssims to -1.
   ssims = jnp.where(jnp.isnan(ssims), -1, ssims)
 
@@ -447,6 +444,12 @@ def ssim(logits, targets, weights=None, mean=None, std=None, volume_max=None):
   ssims = jnp.where(
       jnp.logical_or(ssims > 1, ssims < -1),
       jnp.ones_like(ssims) * -1, ssims)
+
+  # This step should come after setting NaNs to -1. Otherwise,
+  # padding elements with weight value 0 will be set to NaN * 0 = NaN
+  # and then to -1 instead of 0.
+  if weights is not None:
+    ssims = ssims * weights
 
   return ssims
 